3ba7b376a51f23de20e789c3530a878a3cbc9db3
ptaneja <PTRepo>
2014-09-25 11:53:18 -0400

    Initial Commit

---
+++ Selfiephoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Selfiephoto
+//
+//  Created by Tringapps Inc on 9/25/14.
+//  Copyright (c) 2014 TringApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Selfiephoto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Selfiephoto
+//
+//  Created by Tringapps Inc on 9/25/14.
+//  Copyright (c) 2014 TringApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
